bbddf1f3ff5cbcda6944300721b80ee971dc49f2
Emil Bohleber <me@limered.de>
2024-10-07 01:41:15 +0200

    movement and light

---
+++ kids_scaring_kids/src/Player/LightCaster.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Godot;
+using NewGameProject.Animation;
+using NewGameProject.Things;
+using NewGameProject.Utils;
+using NewGameProject.Visibility;
+
+namespace NewGameProject.Player;
+
+public partial class LightCaster : Node2D
+{
+    private ThingsRepository _repo;
+    [Export] public LinearAnimationComponent Animation;
+    [Export] public Node2D Root;
+
+    public override void _Ready()
+    {
+        _repo = GetNode<ThingsRepository>("/root/ThingsRepository");
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var spaceState = GetWorld2D().DirectSpaceState;
+
+        var maxRayLength = 400;
+        var coneAngle = 0.20f;
+        var steps = 200f;
+        var stepSize = coneAngle / steps;
+        var cone = new List<Vector2> { Root.GlobalPosition };
+        for (var i = 0; i < steps; i++)
+        {
+            var angleDiff = stepSize * i - coneAngle * 0.5f;
+            var angle = Animation.RotationAngle + Mathf.Pi * angleDiff - Mathf.Pi;
+            var ray = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+            
+            var query = PhysicsRayQueryParameters2D.Create(Root.Position, ray * maxRayLength + Root.Position);
+            query.CollideWithAreas = true;
+            var result = spaceState.IntersectRay(query);
+            
+            if(result.Count > 0)
+            {
+                cone.Add(result["position"].AsVector2());
+            }
+            else
+            {
+                cone.Add(ray * maxRayLength + Root.Position);
+            }
+        }
+
+        EventBus.Emit(new VisibilityConeChangedEvent { Points = cone });
+    }
+}
---
+++ kids_scaring_kids/src/Player/PlayerControlledBrain.cs
@@ -0,0 +1,25 @@
+﻿using Godot;
+using NewGameProject.Animation;
+
+namespace NewGameProject.Player;
+
+public partial class PlayerControlledBrain : Node2D
+{
+    [Export] public Node2D Root;
+    [Export] public LinearAnimationComponent Animation;
+
+    public override void _Process(double delta)
+    {
+        var direction = Vector2.Zero;
+        if(Input.IsActionPressed("Up")) direction += Vector2.Up;
+        if(Input.IsActionPressed("Left")) direction += Vector2.Left;
+        if(Input.IsActionPressed("Right")) direction += Vector2.Right;
+        if(Input.IsActionPressed("Down")) direction += Vector2.Down;
+        Animation.MovementDirection = direction;
+
+        var mousePosition = GetGlobalMousePosition();
+        var lookDirection = (mousePosition - Root.GlobalPosition).Normalized();
+        var newAngle = Mathf.Pi + Mathf.Atan2(lookDirection.Y, lookDirection.X);
+        Animation.RotationAngle = newAngle;
+    }
+}
