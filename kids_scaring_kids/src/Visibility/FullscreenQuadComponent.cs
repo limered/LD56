6f65d5285876b89f6d99649f8aaf0397ba7c3647
Emil Bohleber <me@limered.de>
2024-10-07 16:04:37 +0200

    add black ground and render lightcone falloff

--- kids_scaring_kids/src/Visibility/FullscreenQuadComponent.cs
+++ kids_scaring_kids/src/Visibility/FullscreenQuadComponent.cs
@@ -6,10 +6,17 @@ public partial class FullscreenQuadComponent : ColorRect
 {
     [Export] public SubViewport ConeViewport;
     [Export] public ShaderMaterial FullscreenQuadShader;
+    [Export] public Node2D Hero; 
 
     public override void _Ready()
     {
         var tex = ConeViewport.GetTexture();
         FullscreenQuadShader.SetShaderParameter("light_cone_texture", tex);
+        
+    }
+
+    public override void _Process(double delta)
+    {
+        FullscreenQuadShader.SetShaderParameter("hero_position", Hero.Position);
     }
 }
---
+++ kids_scaring_kids/src/World/GameWorldComponent.cs
@@ -0,0 +1,8 @@
+﻿using Godot;
+
+namespace NewGameProject.World;
+
+public partial class GameWorldComponent : Node2D
+{
+    [Export] public Vector2 WorldSize;
+}
