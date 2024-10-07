c8168b4fc4bc776522475421766dba9cfbe8282a
Emil Bohleber <me@limered.de>
2024-10-07 12:25:21 +0200

    compose gaem and cone in shader

--- kids_scaring_kids/src/Visibility/VisibilityRendererComponent.cs
+++ kids_scaring_kids/src/Visibility/VisibilityRendererComponent.cs
@@ -6,6 +6,7 @@ namespace NewGameProject.Visibility;
 
 public partial class VisibilityRendererComponent : Node2D
 {
+    [Export] public Polygon2D Polygon2D;
     public List<Vector2> Points = new();
 
     public override void _Ready()
@@ -19,13 +20,16 @@ public partial class VisibilityRendererComponent : Node2D
         Points.AddRange(evt.Points);
     }
 
-    public override void _Draw()
-    {
-        DrawColoredPolygon(Points.ToArray(), new Color(1, 1, 1));
-    }
+    // public override void _Draw()
+    // {
+    //     DrawColoredPolygon(Points.ToArray(), new Color(1, 1, 1));
+    // }
 
     public override void _Process(double delta)
     {
-        QueueRedraw();
+        if(Polygon2D != null)
+        {
+            Polygon2D.Polygon = Points.ToArray();
+        }
     }
 }
