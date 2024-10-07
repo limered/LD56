6531ef08ab0668cef1a616266e7bbbc36d86c780
Emil Bohleber <me@limered.de>
2024-10-07 12:25:21 +0200

    compose gaem and cone in shader

---
+++ kids_scaring_kids/src/Visibility/FullscreenQuadComponent.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+
+namespace NewGameProject.Visibility;
+
+public partial class FullscreenQuadComponent : ColorRect
+{
+    [Export] public SubViewport ConeViewport;
+    [Export] public ShaderMaterial FullscreenQuadShader;
+
+    public override void _Ready()
+    {
+        var tex = ConeViewport.GetTexture();
+        FullscreenQuadShader.SetShaderParameter("light_cone_texture", tex);
+    }
+}
