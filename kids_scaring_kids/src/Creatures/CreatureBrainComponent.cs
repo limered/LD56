8fa6ec3fa9e46495bba3075cf25e04a2cf2343be
limered <me@limered.de>
2024-10-13 01:01:59 +0200

    cleanup code

--- kids_scaring_kids/src/Creatures/CreatureBrainComponent.cs
+++ kids_scaring_kids/src/Creatures/CreatureBrainComponent.cs
@@ -8,23 +8,22 @@ namespace NewGameProject.Creatures;
 
 public partial class CreatureBrainComponent : Node2D
 {
-    [Export] public Node2D Root;
-    [Export] public LinearAnimationComponent Animation;
-    public float TimeInLight = 0.5f;
-    public bool IsFirstCreep = false;
-
-    public bool EyesClosed = false;
-    private Node2D _hero;
-    private ThingsRepository _things;
-    
-    private CreatureState _state = CreatureState.Roaming;
-    private Vector2? _target;
-
     private Vector2 _halfScreenSize;
-    
-    private double _timeToStayHidden;
+    private Node2D _hero;
 
     private StaticOcclusionComponent _lastTree;
+
+    private CreatureState _state = CreatureState.Roaming;
+    private Vector2? _target;
+    private ThingsRepository _things;
+
+    private double _timeToStayHidden;
+    [Export] public LinearAnimationComponent Animation;
+
+    public bool EyesClosed;
+    public bool IsFirstCreep;
+    [Export] public Node2D Root;
+    public float TimeInLight = 0.5f;
     public bool HitRegistered { get; set; }
 
     public override void _Ready()
@@ -46,12 +45,12 @@ public partial class CreatureBrainComponent : Node2D
                 Root.QueueFree();
             }
         }
-        
+
         _hero = _things.Hero;
         if (_hero == null) return;
-        
+
         var distanceToHero = Root.GlobalPosition.DistanceTo(_hero.GlobalPosition);
-        
+
         if (distanceToHero > 320)
         {
             _state = CreatureState.Roaming;
@@ -68,10 +67,7 @@ public partial class CreatureBrainComponent : Node2D
             {
                 Animation.MovementDirection = (_target.Value - Root.GlobalPosition)
                     .Normalized();
-                if (Root.GlobalPosition.DistanceSquaredTo(_target.Value) <= 5f)
-                {
-                    _target = null;
-                }
+                if (Root.GlobalPosition.DistanceSquaredTo(_target.Value) <= 5f) _target = null;
             }
         }
         else
@@ -79,33 +75,30 @@ public partial class CreatureBrainComponent : Node2D
             if (IsFirstCreep)
             {
                 IsFirstCreep = false;
-                EventBus.Emit(new GameStateChangeRequestMsg(){TargetState = GameState.Running});
+                EventBus.Emit(new GameStateChangeRequestMsg { TargetState = GameState.Running });
             }
-            
+
             if (_state == CreatureState.Hiding && _target.HasValue)
             {
                 if (Root.GlobalPosition.DistanceSquaredTo(_target.Value) <= 2f)
                 {
                     EyesClosed = true;
                     Animation.MovementDirection = Vector2.Zero;
-                    
                 }
                 else
                 {
                     Animation.MovementDirection = (_target.Value - Root.GlobalPosition)
                         .Normalized();
                 }
+
                 _timeToStayHidden -= delta;
-                if (_timeToStayHidden <= 0)
-                {
-                    _state = CreatureState.RunAway;
-                }
+                if (_timeToStayHidden <= 0) _state = CreatureState.RunAway;
             }
             else
             {
                 _state = CreatureState.Hiding;
                 _timeToStayHidden = GD.RandRange(2f, 5f);
-                
+
                 var trees = _things.Nodes;
                 var nearest = trees[0];
                 var nearestDistance = nearest.GlobalPosition.DistanceSquaredTo(Root.GlobalPosition);
@@ -119,14 +112,15 @@ public partial class CreatureBrainComponent : Node2D
                         nearest = tree;
                     }
                 }
+
                 var directionToHero = (Root.GlobalPosition - _hero.GlobalPosition).Normalized();
-                _target = nearest.GlobalPosition + (directionToHero * 30f);
+                _target = nearest.GlobalPosition + directionToHero * 30f;
                 _lastTree = nearest;
                 Animation.MovementDirection = (_target.Value - Root.GlobalPosition)
                     .Normalized();
             }
         }
-        
+
         var newX = Mathf.Clamp(Root.GlobalPosition.X, -_halfScreenSize.X, _halfScreenSize.X);
         var newY = Mathf.Clamp(Root.GlobalPosition.Y, -_halfScreenSize.Y, _halfScreenSize.Y);
         Root.GlobalPosition = new Vector2(newX, newY);
