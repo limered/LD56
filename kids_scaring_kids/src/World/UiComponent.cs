e3e8b7bc80a5cd82157fcb5f026ac9a4c4e1b0c9
limered <me@limered.de>
2024-10-13 22:47:51 +0200

    add end screen

--- kids_scaring_kids/src/World/UiComponent.cs
+++ kids_scaring_kids/src/World/UiComponent.cs
@@ -9,16 +9,36 @@ public partial class UiComponent : CanvasLayer
     [Export] public ColorRect ConeDisplay { get; set; }
     [Export] public Button StartButton { get; set; }
     [Export] public RichTextLabel ScoreLabel { get; set; }
+    [Export] public RichTextLabel EndLabel { get; set; }
 
     private ThingsRepository _things;
 
     public override void _Ready()
     {
+        EndLabel.Modulate = new Color(0, 0, 0, 0);
         ConeDisplay.SetVisible(true);
         StartButton.SetVisible(true);
         StartButton.ButtonDown += StartButtonOnButtonDown;
 
         _things = GetNode<ThingsRepository>("/root/ThingsRepository");
+        
+        EventBus.Register<GameStateChangedMsg>(GameStateChanged);
+    }
+
+    private void GameStateChanged(GameStateChangedMsg obj)
+    {
+        if (obj.State is GameState.EndGame)
+        {
+            GetTree().CreateTween()
+                .TweenMethod(Callable.From<float>(ModulateEndLabel), 0f, 1f, 2f)
+                .SetDelay(2f)
+                .SetEase(Tween.EaseType.InOut);
+        }
+    }
+
+    private void ModulateEndLabel(float value)
+    {
+        EndLabel.Modulate = new Color(0, 0, 0, value);
     }
 
     private void StartButtonOnButtonDown()
