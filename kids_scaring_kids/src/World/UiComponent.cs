8e9f6289fd20cee9397dbc6f1567cb94dc16fb19
limered <me@limered.de>
2024-10-13 20:59:26 +0200

    add counter for creeps

--- kids_scaring_kids/src/World/UiComponent.cs
+++ kids_scaring_kids/src/World/UiComponent.cs
@@ -1,4 +1,5 @@
 using Godot;
+using NewGameProject.Things;
 using NewGameProject.Utils;
 
 namespace NewGameProject.World;
@@ -7,17 +8,33 @@ public partial class UiComponent : CanvasLayer
 {
     [Export] public ColorRect ConeDisplay { get; set; }
     [Export] public Button StartButton { get; set; }
+    [Export] public RichTextLabel ScoreLabel { get; set; }
+
+    private ThingsRepository _things;
 
     public override void _Ready()
     {
         ConeDisplay.SetVisible(true);
         StartButton.SetVisible(true);
         StartButton.ButtonDown += StartButtonOnButtonDown;
+
+        _things = GetNode<ThingsRepository>("/root/ThingsRepository");
     }
 
     private void StartButtonOnButtonDown()
     {
         StartButton.SetVisible(false);
         EventBus.Emit(new GameStateChangeRequestMsg { TargetState = GameState.Intro });
+    }
+
+    public override void _Process(double delta)
+    {
+        UpdateLabel();
+    }
+
+    private void UpdateLabel()
+    {
+        var count = _things.Creatures.Count;
+        ScoreLabel.Text = count == 0 ? "" : $"{count}";
     }
 }
